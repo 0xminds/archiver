c03fe33bf499445859453adb480f2a84f6c12553
Ahmed <iaahmed40@gmail.com>
2024-10-29 12:12:10 -0500

    import tx-list is retore from base directory

--- src/Config.ts
+++ src/Config.ts
@@ -193,7 +193,7 @@ let config: Config = {
   firstNodePublicKey: '',
   disableOffloadReceipt: false,
   disableOffloadReceiptForGlobalModification: true,
-  restoreNGTsFromSnapshot: true,
+  restoreNGTsFromSnapshot: false,
 }
 // Override default config params from config file, env vars, and cli args
 export async function overrideDefaultConfig(file: string): Promise<void> {
